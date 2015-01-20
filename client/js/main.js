7c0adca88df22d7fa5975fb5063d972bee0b2ede
despinalr <davidesr7@gmail.com>
2015-01-20 21:33:14 +0000

    list to do

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,11 @@
 angular.module('app', []);
 
 angular.module('app').controller('ctrlTest', function($scope) {
-    $scope.test = "Working!!!";
+    $scope.todos = [{
+        name: 'ToDo 1',
+        description: 'Description 1'
+    }, {
+        name: 'ToDo 2',
+        description: 'Description 2'
+    }];
 });
