3300b0b738aa3186feabd67ca1c91a2c249b9f6e
despinalr <davidesr7@gmail.com>
2015-01-20 19:50:34 +0000

    Initial

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app     = express();
+var cons    = require('consolidate');
+
+app.set('views', __dirname);
+app.engine('.html', cons.swig);
+app.set('view engine', 'html');
+app.use(express.static(__dirname + '/client'));
+
+app.get('*', function(req, res) {
+    res.render('index');
+});
+
+
+app.listen(process.env.PORT, process.env.IP);
