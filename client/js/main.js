5f871566be219c2bd53f412f7d88892fcd835b03
despinalr <davidesr7@gmail.com>
2015-05-13 15:08:53 +0000

    link client wi server api

--- client/js/main.js
+++ client/js/main.js
@@ -1,17 +1,5 @@
-angular.module('app', []);
+angular.module('app', ['ngResource']);
 
-angular.module('app').controller('ctrlTest', function($scope) {
-    $scope.todos = [{
-        name: 'ToDo 1',
-        description: 'Description 1'
-    }, {
-        name: 'ToDo 2',
-        description: 'Description 2'
-    }, {
-        name: 'ToDo 3',
-        description: 'Description 3'
-    }, {
-        name: 'ToDo 4',
-        description: 'Description 4'
-    }];
+angular.module('app').controller('ctrlTest', function($scope, $resource) {
+    $scope.todos = $resource('api/todos').query();
 });
